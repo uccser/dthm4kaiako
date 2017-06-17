4fc2840de3a3ae70142faa78e6904290dd51e88b
Jack Morgan <jackmorgannz@gmail.com>
2017-06-18 08:50:47 +1200

    Add skeleton for Session, Location, and Sponsor models

--- cs4teachers/events/admin.py
+++ cs4teachers/events/admin.py
@@ -1,11 +1,28 @@
 """Administration configuration for the events application."""
 
 from django.contrib import admin
-from events.models import Event
+from events.models import (
+    Event,
+    Session,
+    Location,
+    Sponsor,
+)
 
 
 class EventAdmin(admin.ModelAdmin):
     exclude = ('slug',)
     list_filter = ('is_published',)
 
+
+class SessionAdmin(admin.ModelAdmin):
+    exclude = ('slug',)
+
+
+class LocationAdmin(admin.ModelAdmin):
+    exclude = ('slug',)
+
+
 admin.site.register(Event, EventAdmin)
+admin.site.register(Session, SessionAdmin)
+admin.site.register(Location, LocationAdmin)
+admin.site.register(Sponsor)
--- cs4teachers/events/models.py
+++ cs4teachers/events/models.py
@@ -13,7 +13,6 @@ class Event(models.Model):
     end_date = models.DateField()
     is_published = models.BooleanField(default=False)
 
-
     def save(self, *args, **kwargs):
         """Set slug of object as name upon creation."""
         if not self.id:
@@ -25,5 +24,61 @@ class Event(models.Model):
 
         Returns:
             Name of event (str).
+        """
+        return self.name
+
+
+class Session(models.Model):
+    """Model for session of event."""
+
+    slug = models.SlugField(unique=True)
+    event = models.ForeignKey(
+        Event,
+        on_delete=models.CASCADE,
+        related_name="sessions"
+    )
+    name = models.CharField(max_length=300)
+    description = models.TextField()
+
+    def __str__(self):
+        """Text representation of Session object.
+
+        Returns:
+            Name of session (str).
+        """
+        return self.name
+
+
+class Location(models.Model):
+    """Model for location of session."""
+
+    slug = models.SlugField(unique=True)
+    event = models.ManyToManyField(
+        Session,
+        related_name="locations"
+    )
+    name = models.CharField(max_length=300)
+    description = models.TextField(null=True)
+    address = models.TextField()
+
+    def __str__(self):
+        """Text representation of Location object.
+
+        Returns:
+            Name of location (str).
+        """
+        return self.name
+
+
+class Sponsor(models.Model):
+    """Model for sponsor of event."""
+    name = models.CharField(max_length=200)
+    url = models.URLField()
+
+    def __str__(self):
+        """Text representation of Sponsor object.
+
+        Returns:
+            Name of sponsor (str).
         """
         return self.name
