02b2679c833e09c0d819c06515a06100f135c065
Jack Morgan <jackmorgannz@gmail.com>
2017-06-18 16:55:44 +1200

    Customise admin interface to edit all models

--- cs4teachers/events/admin.py
+++ cs4teachers/events/admin.py
@@ -9,17 +9,47 @@ from events.models import (
 )
 
 
-class EventAdmin(admin.ModelAdmin):
-    exclude = ('slug',)
-    list_filter = ('is_published',)
-
-
 class SessionAdmin(admin.ModelAdmin):
-    exclude = ('slug',)
+    exclude = ("slug",)
+    list_display = ("name", "event")
+    search_fields = ["name", "event"]
+    list_filter = ("event",)
+
+
+class SessionInline(admin.StackedInline):
+    model = Session
+    extra = 5
 
 
 class LocationAdmin(admin.ModelAdmin):
-    exclude = ('slug',)
+    exclude = ("slug",)
+
+
+class EventAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (
+            None,
+            {
+                "fields": ["name", "description"]
+            }
+        ),
+        (
+            "Date information",
+            {
+                "fields": ["start_date", "end_date"]
+            }
+        ),
+        (
+            "Visibility",
+            {
+                "fields": ["is_published"]
+            }
+        ),
+    ]
+    inlines = [SessionInline]
+    list_display = ("name", "start_date", "end_date")
+    list_filter = ("is_published",)
+    search_fields = ["name"]
 
 
 admin.site.register(Event, EventAdmin)
