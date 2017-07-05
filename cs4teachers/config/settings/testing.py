4f289a8ce9cd60ae56144e5552195753a0553008
Jack Morgan <jackmorgannz@gmail.com>
2017-07-06 09:10:14 +1200

    Set default database connection URL for CI testing

--- cs4teachers/config/settings/testing.py
+++ cs4teachers/config/settings/testing.py
@@ -8,7 +8,7 @@ from .base import *  # noqa: F403
 # ----------------------------------------------------------------------------
 # See: https://docs.djangoproject.com/en/dev/ref/settings/#databases
 DATABASES = {
-    'default': env.db('DATABASE_URL'),  # noqa: F405
+    "default": env.db("DATABASE_URL", default="postgres:///cs4teachers"),  # noqa: F405
 }
 DATABASES['default']['ATOMIC_REQUESTS'] = True
 
