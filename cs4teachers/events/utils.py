01554207266e04032a8463dad10317db83446213
Jack Morgan <jackmorgannz@gmail.com>
2017-07-15 08:27:28 +1200

    Calculate days difference for each event

--- cs4teachers/events/utils.py
+++ cs4teachers/events/utils.py
@@ -1,6 +1,6 @@
 """Utility functions for the events application."""
 
-from datetime import datetime
+from datetime import datetime, date
 from django.db.models import DateField
 from django.db.models.aggregates import Max, Min
 from events.models import (
@@ -31,7 +31,20 @@ class GenericEvent:
         self.end_date = end_date
         self.series = series
         self.third_party = third_party
+        self.days_difference = self.calculate_days_difference()
 
+    def calculate_days_difference(self):
+        today = date.today()
+        # If event is on now
+        if today >= self.start_date and today <= self.end_date:
+            days_difference = 0
+        # Otherwise, next upcoming event
+        elif today > self.end_date:
+            days_difference = (self.end_date - today).days
+        # Otherwise, latest event
+        else:
+            days_difference = (self.start_date - today).days
+        return days_difference
 
 def retrieve_all_events(upcoming=False, series=None):
     """Retrieve both events and third party events.
@@ -47,7 +60,6 @@ def retrieve_all_events(upcoming=False, series=None):
         List of event objects (list).
     """
     all_events = []
-    today = datetime.today()
 
     events = Event.objects.filter(
         is_published=True
@@ -58,6 +70,7 @@ def retrieve_all_events(upcoming=False, series=None):
     )
 
     if upcoming:
+        today = datetime.today()
         events = events.filter(end_date__gte=today)
         third_party_events = third_party_events.filter(end_date__gte=today)
 
