140b72300baa1fbf4b2b991be62995d286baa677
Jack Morgan <jackmorgannz@gmail.com>
2019-01-21 12:09:14 +1300

    Correct path for decrypting secrets

--- infrastructure/prod-deploy/decrypt-prod-secrets.sh
+++ infrastructure/prod-deploy/decrypt-prod-secrets.sh
@@ -5,7 +5,7 @@
 # This includes:
 #   - Google Cloud Platform Service Account for using gcloud.
 #   - Script tp load environment variables used for running Django in production.
-openssl aes-256-cbc -K "${encrypted_adab45d1d2ed_key}" -iv "${encrypted_adab45d1d2ed_iv}" -in ./infrastructure/prod-deploy/prod-deploy-secrets.tar.enc -out prod-deploy-secrets.tar -d
+openssl aes-256-cbc -K "${encrypted_adab45d1d2ed_key}" -iv "${encrypted_adab45d1d2ed_iv}" -in ./infrastructure/prod-deploy/prod-deploy-secrets.tar.enc -out ./dthm4kaiako/prod-deploy-secrets.tar -d
 
 # Unzip the decrypted secret archive.
-tar -C ./dthm4kaiako/dthm4kaiako/ -xf prod-deploy-secrets.tar
+tar -C ./dthm4kaiako/ -xf prod-deploy-secrets.tar
