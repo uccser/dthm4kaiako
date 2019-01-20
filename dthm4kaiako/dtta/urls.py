6c6232a8d79a205f5de151a9c2e51199c2b7fe57
Jack Morgan <jackmorgannz@gmail.com>
2019-01-20 23:45:30 +1300

    Add basic DTTA website pages

--- dthm4kaiako/dtta/urls.py
+++ dthm4kaiako/dtta/urls.py
@@ -6,5 +6,8 @@ from . import views
 app_name = 'dtta'
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
+    path('about/', views.AboutView.as_view(), name='about'),
+    path('membership/', views.MembershipView.as_view(), name='membership'),
+    path('news/', views.NewsArticleListView.as_view(), name='news_article_list'),
     path('news/<int:pk>/', views.NewsArticleDetailView.as_view(), name='news_article'),
 ]
