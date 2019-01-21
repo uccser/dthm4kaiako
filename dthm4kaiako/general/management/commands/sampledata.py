22fa50775ad950a8d087aa1875968af62c9b5bc7
Jack Morgan <jackmorgannz@gmail.com>
2019-01-21 15:10:57 +1300

    Docstring fix

--- dthm4kaiako/general/management/commands/sampledata.py
+++ dthm4kaiako/general/management/commands/sampledata.py
@@ -13,8 +13,6 @@ class Command(management.base.BaseCommand):
 
     def handle(self, *args, **options):
         """Automatically called when the sampledata command is given."""
-
-        # Only run in debug mode
         if not settings.DEBUG:
             raise management.base.CommandError(
                 'This command can only be executed in DEBUG mode.'
