ca4d68ed35adcdfeb49067d554032ddc5efc1764
Jack Morgan <jackmorgannz@gmail.com>
2019-01-28 12:10:54 +1300

    Remove unused import

--- dthm4kaiako/config/urls.py
+++ dthm4kaiako/config/urls.py
@@ -4,7 +4,6 @@ from django.conf import settings
 from django.urls import include, path
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.views.generic import TemplateView
 from django.views import defaults as default_views
 from config.views import health_check
 
