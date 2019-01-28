1e3d4e44053f0b53dc8b51c6597b07b82c4ee8f9
Jack Morgan <jackmorgannz@gmail.com>
2019-01-28 15:06:14 +1300

    Only output analytics code in production env

---
+++ dthm4kaiako/config/context_processors/deployed.py
@@ -0,0 +1,13 @@
+"""Context processor for checking if in deployed environment."""
+
+from django.conf import settings
+
+
+def deployed(request):
+    """Return a dictionary containing boolean if deployed environment.
+    Returns:
+        Dictionary containing deployed boolean to add to context.
+    """
+    return {
+        "DEPLOYED": settings.DJANGO_PRODUCTION
+    }
