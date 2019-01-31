b09e93f2ac3a8717710fdc1640f9dd9f77bb2c37
Jack Morgan <jackmorgannz@gmail.com>
2019-01-31 16:11:34 +1300

    Automatically verify email address for sampledata command

--- dthm4kaiako/general/management/commands/sampledata.py
+++ dthm4kaiako/general/management/commands/sampledata.py
@@ -3,6 +3,7 @@
 from django.core import management
 from django.conf import settings
 from django.contrib.auth import get_user_model
+from allauth.account.models import EmailAddress
 from tests.dtta.factories import (
     NewsArticleFactory,
     PageFactory,
@@ -25,11 +26,25 @@ class Command(management.base.BaseCommand):
         # Clear all data
         management.call_command('flush', interactive=False)
 
-        # Create admin account
         User = get_user_model()
-        User.objects.filter(email='admin@example.com').delete()
-        User.objects.create_superuser('admin', 'admin@example.com', 'password')
-        User.objects.create_user('user', email='user@example.com', password='password', first_name='Alex', last_name='Doe')
+
+        # Create admin account
+        admin = User.objects.create_superuser('admin', 'admin@example.com', 'password', first_name='Admin', last_name='Account')
+        EmailAddress.objects.create(
+            user=admin,
+            email=admin.email,
+            primary=True,
+            verified=True
+        )
+
+        # Create user account
+        alex = User.objects.create_user('user', 'user@example.com', password='password', first_name='Alex', last_name='Doe')
+        EmailAddress.objects.create(
+            user=alex,
+            email=alex.email,
+            primary=True,
+            verified=True
+        )
 
         # DTTA
         NewsArticleFactory.create_batch(size=20)
