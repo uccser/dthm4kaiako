3e163969c920ef5214693abcecabb1ab132612da
Jack Morgan <jackmorgannz@gmail.com>
2019-02-02 17:09:12 +1300

    Create models for DTTA news article sources and audiences

--- dthm4kaiako/dtta/models.py
+++ dthm4kaiako/dtta/models.py
@@ -4,6 +4,7 @@ from django.db import models
 from django.urls import reverse
 from autoslug import AutoSlugField
 from ckeditor_uploader.fields import RichTextUploadingField
+from utils.get_upload_filepath import get_dtta_news_article_source_upload_path
 
 
 class Page(models.Model):
@@ -33,6 +34,39 @@ class Page(models.Model):
         return self.title
 
 
+class NewsArticleAudience(models.Model):
+    """Model for an audience of a news article published by DTTA."""
+
+    name = models.CharField(max_length=200)
+    slug = AutoSlugField(populate_from='name', always_update=True, unique=True)
+    colour = models.CharField(max_length=20)
+
+    def __str__(self):
+        """Text representation of object.
+
+        Returns:
+            Name of news article audience (str).
+        """
+        return self.name
+
+
+class NewsArticleSource(models.Model):
+    """Model for a source of a news article published by DTTA."""
+
+    name = models.CharField(max_length=200)
+    slug = AutoSlugField(populate_from='name', always_update=True, unique=True)
+    logo = models.ImageField(null=True, blank=True, upload_to=get_dtta_news_article_source_upload_path)
+    website = models.URLField(blank=True)
+
+    def __str__(self):
+        """Text representation of object.
+
+        Returns:
+            Name of news article source (str).
+        """
+        return self.name
+
+
 class NewsArticle(models.Model):
     """Model for a news article published by DTTA."""
 
@@ -40,6 +74,18 @@ class NewsArticle(models.Model):
     slug = AutoSlugField(populate_from='title', always_update=True, null=True)
     datetime = models.DateTimeField()
     content = RichTextUploadingField()
+    source = models.ForeignKey(
+        NewsArticleSource,
+        on_delete=models.CASCADE,
+        related_name='news_articles',
+        null=True,
+        blank=True,
+    )
+    audiences = models.ManyToManyField(
+        NewsArticleAudience,
+        related_name='news_articles',
+        blank=True,
+    )
 
     def get_absolute_url(self):
         """Return URL of object on website.
