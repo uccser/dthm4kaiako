7f8859260f7fb0ad93369de6c5237359307f4199
JackMorganNZ <jackmorgannz@gmail.com>
2019-08-09 14:31:05 +1200

    Fix division by zero error

--- dthm4kaiako/poet/views.py
+++ dthm4kaiako/poet/views.py
@@ -158,7 +158,10 @@ class StatisticsDetailsView(PermissionRequiredMixin, DetailView):
         progress_outcomes = {x.code: x for x in ProgressOutcome.objects.annotate(
             count=Count('submissions', filter=Q(submissions__resource=self.object)))}
         for progress_outcome_code, progress_outcome in progress_outcomes.items():
-            progress_outcome.percentage = progress_outcome.count / total_submissions
+            if total_submissions:
+                progress_outcome.percentage = progress_outcome.count / total_submissions
+            else:
+                progress_outcome.percentage = 0
         context['total_submissions'] = total_submissions
         context['progress_outcomes'] = progress_outcomes
         context['progress_outcome_widget'] = 'poet/widgets/progress-outcome-radio-statistics.html'
