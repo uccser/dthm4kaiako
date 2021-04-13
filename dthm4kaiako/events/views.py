585d9668db25cd8c9c384dd1e28b77742f5448b7
Jack Morgan <jackmorgannz@gmail.com>
2021-04-13 16:26:20 +1200

    Reorder schedule data to allow easier templating
    Also allows setting certain elements
    with sticky behaviour with expected
    behaviours.

--- dthm4kaiako/events/views.py
+++ dthm4kaiako/events/views.py
@@ -9,7 +9,7 @@ from events.models import (
     Location,
 )
 from events.filters import UpcomingEventFilter, PastEventFilter
-from events.utils import create_filter_helper
+from events.utils import create_filter_helper, organise_schedule_data
 
 
 class HomeView(generic.TemplateView):
@@ -112,7 +112,9 @@ class EventDetailView(RedirectToCosmeticURLMixin, generic.DetailView):
         context = super().get_context_data(**kwargs)
         context['sponsors'] = self.object.sponsors.all()
         context['organisers'] = self.object.organisers.all()
-        context['sessions'] = self.object.sessions.all().prefetch_related('locations')
+        context['schedule'] = organise_schedule_data(
+            self.object.sessions.all().prefetch_related('locations')
+        )
         context['locations'] = self.object.locations.all()
         return context
 
