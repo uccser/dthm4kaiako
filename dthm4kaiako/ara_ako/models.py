c9ae78238bf643025b462e8c062a0e20d3b61715
Jack Morgan <jackmorgannz@gmail.com>
2021-04-17 21:56:24 +1200

    Add basic Ara Ako application

---
+++ dthm4kaiako/ara_ako/models.py
@@ -0,0 +1,56 @@
+"""Models for Ara Ako application."""
+
+from django.db import models
+from django.urls import reverse
+from ckeditor_uploader.fields import RichTextUploadingField
+from events.models import Event
+from resources.models import Resource
+
+
+class AraAkoEvent(models.Model):
+    """Model for an Ara Ako event."""
+
+    slug = models.SlugField()
+    published = models.BooleanField(default=False)
+    event = models.OneToOneField(
+        Event,
+        verbose_name='Event',
+        related_name='ara_ako_event',
+        on_delete=models.CASCADE,
+    )
+
+    def get_absolute_url(self):
+        """Return URL of Ara Ako event on website.
+
+        Returns:
+            URL as a string.
+        """
+        return reverse('ara_ako:event', kwargs={'slug': self.slug})
+
+    def __str__(self):
+        """Text representation of an Ara Ako event."""
+        return self.event.name
+
+    class Meta:
+        """Meta options for class."""
+
+        ordering = ['-event__start', ]
+
+
+class AraAkoTeam(models.Model):
+    """Model for an Ara Ako team."""
+
+    number = models.PositiveSmallIntegerField()
+    description = RichTextUploadingField()
+    resource = models.OneToOneField(
+        Resource,
+        verbose_name='Ara Ako team resource',
+        related_name='ara_ako_team',
+        on_delete=models.CASCADE,
+    )
+    event = models.ForeignKey(
+        AraAkoEvent,
+        verbose_name='Ara Ako teams',
+        related_name='teams',
+        on_delete=models.CASCADE,
+    )
--- dthm4kaiako/ara_ako/urls.py
+++ dthm4kaiako/ara_ako/urls.py
@@ -5,5 +5,8 @@ from ara_ako import views
 
 app_name = 'ara_ako'
 urlpatterns = [
-    path('', views.HomeView.as_view(), name='home'),
+    path('', views.AraAkoHomeView.as_view(), name='home'),
+    path('json/dashboard/', views.dashboard_json, name='dashboard_json'),
+    path('<slug:slug>/', views.AraAkoEventDetailView.as_view(), name='event'),
+    path('<slug:slug>/dashboard/', views.AraAkoDashboardView.as_view(), name='dashboard'),
 ]
