e3415cd9c8f1f3c44a4d05da2be40407a10742b1
HayleyKrippner <hek21@uclive.ac.nz>
2022-07-09 18:24:59 +1200

    Adding unit tests for test_urls

--- dthm4kaiako/tests/events/test_urls.py
+++ dthm4kaiako/tests/events/test_urls.py
@@ -1,65 +1,58 @@
+from django.test import TestCase
 from django.urls import reverse, resolve
+import events
 
-class EventURLTest():
+class EventURLTest(TestCase):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
 
-    def test_upcoming__reverse_provides_correct_url():
-        assert(reverse("events:upcoming") == "/events/upcoming")
+    def test_upcoming__reverse_provides_correct_url(self):
+        self.assertEqual(reverse("events:upcoming"), "/events/upcoming/")
 
 
-    def test_upcoming__resolve_provides_correct_view_name():
-        assert(resolve("/events/upcoming/").view_name == "events:upcoming")
+    def test_upcoming__resolve_provides_correct_view_name(self):
+        self.assertEqual(resolve("/events/upcoming/").view_name, "events:upcoming")
 
 
-    def test_past__reverse_provides_correct_url():
-        assert(reverse("events:past") == "/events/past")
+    def test_past__reverse_provides_correct_url(self):
+        self.assertEqual(reverse("events:past"), "/events/past/")
 
 
-    def test_past__resolve_provides_correct_view_name():
-        assert(resolve("/events/past/").view_name == "events:past")
+    def test_past__resolve_provides_correct_view_name(self):
+        self.assertEqual(resolve("/events/past/").view_name, "events:past")
+
+    # TODO: fix - Failing
+    def test_event_detail__reverse_provides_correct_url_with_slug(self):
+        pk = 1,
+        slug = "CS-conference"
+        self.assertEqual(reverse('events:event', args=[pk, slug]), "/events/event/1/CS-conference/")
 
 
-    # def test_event_detail__reverse_provides_correct_url_with_slug():
-    #     kwargs = {
-    #         pk = 1,
-    #         slug = "CS-conference",
-    #     }
-    #     url = reverse("events:event", kwargs=kwargs)
-    #     self.assertEqual(url, "/events/1/CS-conference/")
-
-    
-    def test_event_detail__resolve_provides_correct_view_name_with_slug():
-        pass
+    # TODO: fix - Failing
+    def test_location(self):
+        pk = 1,
+        kwargs = {'pk' : pk}
+        url = reverse("events:location", kwargs=kwargs)
+        self.assertEqual(url, "events/location/1/")
 
 
-    # def test_location():
-    #     kwargs = {
-    #         pk = 1,
-    #     }
-    #     url = reverse("events:location", kwargs=kwargs)
-    #     self.assertEqual(url, "/location/1/")
+    def test_applications__reverse_provides_correct_url(self):
+        self.assertEqual(reverse("events:event_applications"), "/events/applications/")
 
 
-    def test_applications__reverse_provides_correct_url():
-        assert(reverse("events:applications") == "/events/applications")
+    def test_applications__resolve_provides_correct_view_name(self):
+        self.assertEqual(resolve("/events/applications/").view_name, "events:event_applications")
 
 
-    def test_applications__resolve_provides_correct_view_name():
-        assert(resolve("/events/applications/").view_name == "events:applications")
+    def test_register__reverse_provides_correct_url(self):
+        kwargs = {'pk' : 1}
+        url = reverse("events:apply", kwargs=kwargs)
+        self.assertEqual(url, "/events/register/1/")
 
 
-    # def test_register__reverse_provides_correct_url():
-    #     kwargs = {
-    #         pk = 1,
-    #     }
-    #     url = reverse("events:register", kwargs=kwargs)
-    #     self.assertEqual(url, "/events/register/1/")
-
-
-    def test_register__resolve_provides_correct_view_name():
+    def test_register__resolve_provides_correct_view_name(self):
         pass
     
     
