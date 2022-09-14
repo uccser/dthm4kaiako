f0bfaa6e480cbe07afb852d1af2f10ddc89f6a90
HayleyKrippner <hek21@uclive.ac.nz>
2022-09-14 19:40:08 +1200

    Event applications ordered most recent and categorised by past and upcoming for teacher view

--- dthm4kaiako/events/views.py
+++ dthm4kaiako/events/views.py
@@ -216,24 +216,17 @@ class EventApplicationsView(LoginRequiredMixin, generic.ListView):
         context = super().get_context_data(**kwargs)
         user = self.request.user
 
-# event__start__gte=today
-# event__start__lte=today
-
         if user.is_authenticated:
             today = datetime.today()
             event_applications_upcoming = EventApplication.objects.filter(user=user, event__start__gte=today).order_by(
                 'event__start')
-            event_applications_future = EventApplication.objects.filter(user=user, event__start__lte=today).order_by(
-                'event__start')
+            event_applications_past = EventApplication.objects.filter(user=user, event__start__lte=today).order_by(
+                '-event__start')
             context['event_applications_upcoming'] = event_applications_upcoming
-            context['event_applications_future'] = event_applications_future
+            context['event_applications_past'] = event_applications_past
             context['user'] = self.request.user
             if len(event_applications_upcoming) != 0:
                 context['withdraw_event_application_form'] = WithdrawEventApplicationForm(self.request.POST)
-
-        messages.warning(self.request, event_applications_upcoming)
-        messages.warning(self.request, event_applications_future)
-
         return context
 
     def get_object(self):
