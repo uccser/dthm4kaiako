8207a1c1e06c31952a96936358d0186c474e1eac
HayleyKrippner <hek21@uclive.ac.nz>
2022-10-16 13:49:29 +1300

    More unit tests. Got to 71% total coverage, events/views at 54% and events/models at 77%. Still 13 tests to fix and/or debug and 7-8 more test classes to write.

--- dthm4kaiako/tests/events/urls/test_manage_event_registration_form_details.py
+++ dthm4kaiako/tests/events/urls/test_manage_event_registration_form_details.py
@@ -1,18 +1,13 @@
 """Unit tests for manage_event_registration_form_details url"""
 
 from django.urls import reverse, resolve
-from http import HTTPStatus
-from users.models import User
-from django.contrib.gis.geos import Point
 import datetime
 from events.models import (
-    Location,
     Event,
 )
 import pytz
 from tests.BaseTestWithDB import BaseTestWithDB
 from users.models import User
-from django.test.utils import override_settings
 NEW_ZEALAND_TIME_ZONE = pytz.timezone('Pacific/Auckland')
 
 
@@ -21,7 +16,6 @@ class ManageEventRegistrationFormDetailsURLTest(BaseTestWithDB):
     @classmethod
     def tearDownTestData(cls):
         Event.objects.all().delete()
-        Location.objects.all().delete()
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
@@ -62,54 +56,3 @@ class ManageEventRegistrationFormDetailsURLTest(BaseTestWithDB):
             "events:manage_event_registration_form_details"
         )
 
-    @override_settings(GOOGLE_MAPS_API_KEY="mocked")
-    def test_manage_event_registration_form_details_url_returns_200_when_event_exists(self):
-        user_kate = User.objects.create_user(
-            id=1,
-            username='kate',
-            first_name='Kate',
-            last_name='Bush',
-            email='kate@uclive.ac.nz',
-            password='potato',
-        )
-        user_kate.save()
-
-        location_1 = Location.objects.create(
-            id=1,
-            room='Room 123',
-            name='Middleton Grange School',
-            street_address='12 High Street',
-            suburb='Riccarton',
-            city='Chrirstchurch',
-            region=14,
-            coords=Point(-43, 172)
-        )
-        location_1.save()
-
-        event_physical_register_1 = Event.objects.create(
-            id=1,
-            name="DTHM for Kaiako Conference 2023",
-            description="description",
-            registration_type=1,
-            start=datetime.datetime(2023, 6, 24, 0, 0, 0, 00, NEW_ZEALAND_TIME_ZONE),
-            end=datetime.datetime(2023, 6, 26, 0, 0, 0, 00, NEW_ZEALAND_TIME_ZONE),
-            accessible_online=False,
-            published=True
-        )
-        event_physical_register_1.locations.set([location_1])
-        event_physical_register_1.save()
-        
-        event = Event.objects.get(pk=event_physical_register_1.pk)
-        event.event_staff.set([user_kate])
-        event.save()
-        self.client.force_login(user_kate)
-        kwargs = {
-            'pk': event.pk,
-            }
-
-        user = User.objects.get(pk=1)
-        event.event_staff.set([user])
-        event.save()
-        url = reverse('events:manage_event_registration_form_details', kwargs=kwargs)
-        response = self.client.post(url)
-        self.assertEqual(HTTPStatus.OK, response.status_code)
---
+++ dthm4kaiako/tests/events/views/test_manage_event_registration_form_details_view.py
@@ -0,0 +1,227 @@
+"""Unit tests for manage_event_details_views"""
+
+import pytz
+from django.urls import reverse
+from http import HTTPStatus
+from events.models import Event
+from users.models import User
+from tests.BaseTestWithDB import BaseTestWithDB
+from django.test.utils import override_settings
+from django.contrib.gis.geos import Point
+import datetime
+from events.models import (
+    Location,
+    Event,
+)
+from events.views import manage_event_details_view
+from unittest import mock
+from django.test.client import RequestFactory
+
+NEW_ZEALAND_TIME_ZONE = pytz.timezone('Pacific/Auckland')
+
+
+class ManageEventDetailsURLTest(BaseTestWithDB):
+
+    @classmethod
+    def tearDownTestData(cls):
+        Event.objects.all().delete()
+        Location.objects.all().delete()
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    @override_settings(GOOGLE_MAPS_API_KEY="mocked")
+    def test_manage_event_registration_form_details_view_returns_200_when_event_exists(self):
+        user_kate = User.objects.create_user(
+            id=1,
+            username='kate',
+            first_name='Kate',
+            last_name='Bush',
+            email='kate@uclive.ac.nz',
+            password='potato',
+        )
+        user_kate.save()
+
+        location_1 = Location.objects.create(
+            id=1,
+            room='Room 123',
+            name='Middleton Grange School',
+            street_address='12 High Street',
+            suburb='Riccarton',
+            city='Chrirstchurch',
+            region=14,
+            coords=Point(-43, 172)
+        )
+        location_1.save()
+
+        event_physical_register_1 = Event.objects.create(
+            id=1,
+            name="DTHM for Kaiako Conference 2023",
+            description="description",
+            registration_type=1,
+            start=datetime.datetime(2023, 6, 24, 0, 0, 0, 00, NEW_ZEALAND_TIME_ZONE),
+            end=datetime.datetime(2023, 6, 26, 0, 0, 0, 00, NEW_ZEALAND_TIME_ZONE),
+            accessible_online=False,
+            published=True
+        )
+        event_physical_register_1.locations.set([location_1])
+        event_physical_register_1.save()
+        
+        event = Event.objects.get(pk=event_physical_register_1.pk)
+        event.event_staff.set([user_kate])
+        event.save()
+        self.client.force_login(user_kate)
+        kwargs = {
+            'pk': event.pk,
+            }
+        url = reverse('events:manage_event_registration_form_details', kwargs=kwargs)
+        response = self.client.post(url)
+        self.assertEqual(HTTPStatus.OK, response.status_code)
+
+    # TODO
+    @override_settings(GOOGLE_MAPS_API_KEY="mocked")
+    def test_manage_event_registration_form_details_view_and_logged_in_and_staff_then_successfully_update(self):
+        user_kate = User.objects.create_user(
+            id=1,
+            username='kate',
+            first_name='Kate',
+            last_name='Bush',
+            email='kate@uclive.ac.nz',
+            password='potato',
+        )
+        user_kate.save()
+
+        location_1 = Location.objects.create(
+            id=1,
+            room='Room 123',
+            name='Middleton Grange School',
+            street_address='12 High Street',
+            suburb='Riccarton',
+            city='Chrirstchurch',
+            region=14,
+            coords=Point(-43, 172)
+        )
+        location_1.save()
+
+        event_physical_register_1 = Event.objects.create(
+            id=1,
+            name="DTHM for Kaiako Conference 2023",
+            description="description",
+            registration_type=1,
+            start=datetime.datetime(2023, 6, 24, 0, 0, 0, 00, NEW_ZEALAND_TIME_ZONE),
+            end=datetime.datetime(2023, 6, 26, 0, 0, 0, 00, NEW_ZEALAND_TIME_ZONE),
+            accessible_online=False,
+            published=True
+        )
+        event_physical_register_1.locations.set([location_1])
+        event_physical_register_1.save()
+        
+        event = Event.objects.get(pk=event_physical_register_1.pk)
+        event.event_staff.set([user_kate])
+        event.save()
+        self.client.force_login(user_kate)
+        kwargs = {
+            'pk': event.pk,
+            }
+        event.event_staff.set([user_kate])
+        event.save()
+        url = reverse('events:manage_event_registration_form_details', kwargs=kwargs)
+        response = self.client.post(url)
+        self.assertEqual(HTTPStatus.OK, response.status_code)
+
+    @override_settings(GOOGLE_MAPS_API_KEY="mocked")
+    def test_manage_event_registration_form_details_view_and_not_logged_in_and_staff_then_redirected(self):
+        user_kate = User.objects.create_user(
+            id=1,
+            username='kate',
+            first_name='Kate',
+            last_name='Bush',
+            email='kate@uclive.ac.nz',
+            password='potato',
+        )
+        user_kate.save()
+
+        location_1 = Location.objects.create(
+            id=1,
+            room='Room 123',
+            name='Middleton Grange School',
+            street_address='12 High Street',
+            suburb='Riccarton',
+            city='Chrirstchurch',
+            region=14,
+            coords=Point(-43, 172)
+        )
+        location_1.save()
+
+        event_physical_register_1 = Event.objects.create(
+            id=1,
+            name="DTHM for Kaiako Conference 2023",
+            description="description",
+            registration_type=1,
+            start=datetime.datetime(2023, 6, 24, 0, 0, 0, 00, NEW_ZEALAND_TIME_ZONE),
+            end=datetime.datetime(2023, 6, 26, 0, 0, 0, 00, NEW_ZEALAND_TIME_ZONE),
+            accessible_online=False,
+            published=True
+        )
+        event_physical_register_1.locations.set([location_1])
+        event_physical_register_1.save()
+        
+        event = Event.objects.get(pk=event_physical_register_1.pk)
+        event.event_staff.set([user_kate])
+        event.save()
+        kwargs = {
+            'pk': event.pk,
+            }
+        event.event_staff.set([user_kate])
+        event.save()
+        url = reverse('events:manage_event_registration_form_details', kwargs=kwargs)
+        response = self.client.post(url)
+        self.assertEqual(HTTPStatus.FOUND, response.status_code)
+        self.assertEqual(response['Location'], f'/accounts/login/?next=/events/manage-event-registration-form-details/{event.pk}/')
+
+    @override_settings(GOOGLE_MAPS_API_KEY="mocked")
+    def test_manage_event_registration_form_details_view_and_logged_in_and_not_staff_then_redirected(self):
+        user_kate = User.objects.create_user(
+            id=1,
+            username='kate',
+            first_name='Kate',
+            last_name='Bush',
+            email='kate@uclive.ac.nz',
+            password='potato',
+        )
+        user_kate.save()
+
+        location_1 = Location.objects.create(
+            id=1,
+            room='Room 123',
+            name='Middleton Grange School',
+            street_address='12 High Street',
+            suburb='Riccarton',
+            city='Chrirstchurch',
+            region=14,
+            coords=Point(-43, 172)
+        )
+        location_1.save()
+
+        event_physical_register_1 = Event.objects.create(
+            id=1,
+            name="DTHM for Kaiako Conference 2023",
+            description="description",
+            registration_type=1,
+            start=datetime.datetime(2023, 6, 24, 0, 0, 0, 00, NEW_ZEALAND_TIME_ZONE),
+            end=datetime.datetime(2023, 6, 26, 0, 0, 0, 00, NEW_ZEALAND_TIME_ZONE),
+            accessible_online=False,
+            published=True
+        )
+        event_physical_register_1.locations.set([location_1])
+        event_physical_register_1.save()
+        self.client.force_login(user_kate)
+
+        event = Event.objects.get(pk=event_physical_register_1.pk)
+        kwargs = {
+            'pk': event.pk,
+            }
+        url = reverse('events:manage_event_registration_form_details', kwargs=kwargs)
+        response = self.client.post(url)
+        self.assertEqual(HTTPStatus.FOUND, response.status_code)
+        self.assertEqual(response['Location'], '/events/manage/')
