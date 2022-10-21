5fcb1a2fc4a197b0b2438fc205e8379647d160b7
HayleyKrippner <hek21@uclive.ac.nz>
2022-10-21 14:30:37 +1300

    Fixing manage registration form url issue

--- dthm4kaiako/events/views.py
+++ dthm4kaiako/events/views.py
@@ -974,9 +974,9 @@ def manage_event_registration_form_details_view(request, pk):
     context['new_participant_form'] = ParticipantTypeCreationForm()
     context['update_participant_form'] = ParticipantTypeCreationForm()
 
-    # manage_registration_form_details_form = manage_registration_form_details_form = ManageEventRegistrationFormDetailsForm(
-    #         instance=registration_form
-    #         )
+    manage_registration_form_details_form = ManageEventRegistrationFormDetailsForm(
+            instance=registration_form
+            )
 
     if request.method == 'POST':
         manage_registration_form_details_form = ManageEventRegistrationFormDetailsForm(
